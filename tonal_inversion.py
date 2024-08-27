2ce004a259e0d9dab44ce5798da1dd45c2607307
Duncan Chuntey <louisclifford@gmail.com>
2024-08-27 15:25:06 +0200

    Added notes list, beginnings of a scale dicitonary, differentiting between scale and mode inputs, added get_inversion_axis() function. Pukka.

--- tonal_inversion.py
+++ tonal_inversion.py
@@ -1,5 +1,7 @@
 import re
 
+notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
 major_keys = [
     ["C", "D", "E", "F", "G", "A", "B"],
     ["G", "A", "B", "C", "D", "E", "F#"],
@@ -20,7 +22,7 @@ major_keys = [
 
 pattern = r"[A-G](?:[#b])?"
 
-scales_modes = [
+modes = [
     "major",
     "dorian",
     "phrygian",
@@ -30,33 +32,58 @@ scales_modes = [
     "locrian",
 ]
 
+scales = {
+    "whole-tone": [0, 2, 4, 6, 8, 10],
+    "acoustic scale": [0, 2, 4, 6, 7, 9, 10],
+    "lydian dominant": [0, 2, 4, 6, 7, 9, 10],
+    # Changed 9 to 8 (G# instead of Ab, shrug emoji)
+    "algerian": [0, 2, 3, 6, 7, 8, 11, 12, 14, 15, 17],
+    # Testing custom pitch sets.
+    "custom": [0, 1, 5, 6, 8, 9, 12, 14]
+}
 
 def get_scale():
     root = re.findall(pattern, input("Root: "))[0]
 
     scale = input("Scale/mode: ").lower().strip()
-    scale_mode_idx = scales_modes.index(scale)
 
     if scale == "aeolian":
         scale = "minor"
 
-    if scale == "ionian":
+    elif scale == "ionian":
         scale = "major"
 
-    tonality = [notes for notes in major_keys if root == notes[scale_mode_idx]]
-    tonality = tonality[0]
-    rotate_to_scale = tonality[scale_mode_idx:] + tonality[:scale_mode_idx]
-    
-    print(f"Chosen tonality: {", ".join(map(str, rotate_to_scale))}")
-    
-    inversion_axis = re.findall(pattern, input("Axis: "))[0]
-    axis_idx = rotate_to_scale.index(inversion_axis)
+    if scale in modes:
+        scale_mode_idx = modes.index(scale)
+        tonality = [n for n in major_keys if root == n[scale_mode_idx]]
+        tonality = tonality[0]
+        rotated_tonality = tonality[scale_mode_idx:] + tonality[:scale_mode_idx]
+        print(f"Chosen tonality: {", ".join(map(str, rotated_tonality))}")
+        inversion_axis = get_inversion_axis()
+        axis_idx = rotated_tonality.index(inversion_axis)
+        rotated_to_axis = rotated_tonality[axis_idx:] + rotated_tonality[:axis_idx]
+
+# TODO: For my "skeletons", rotated_to_root also needs to be transposed 
+# to the relevant strings in cases of multiple string groupings.
+# Two tonality variables may be necessary and then require concatenation.
+    else:
+        if scale_set := scales.get(scale):
+            root_idx = notes.index(root)
+            rotated_to_root = notes[root_idx:] + notes[:root_idx]
+            tonality = [rotated_to_root[i % 12] for i in scale_set]
+            print(f"Chosen tonality: {", ".join(tonality)}")
+            inversion_axis = get_inversion_axis()
+            axis_idx = tonality.index(inversion_axis)
+            rotated_to_axis = tonality[axis_idx:] + tonality[:axis_idx]
 
     notes_in = re.findall(pattern, input("Chord/notes: "))
 
-    rotated_to_axis = rotate_to_scale[axis_idx:] + rotate_to_scale[:axis_idx]
-
     return rotated_to_axis, notes_in
+
+
+def get_inversion_axis():
+    inversion_axis = re.findall(pattern, input("Axis: "))[0]
+    return inversion_axis
 
 
 def tonal_inversion(rotated_to_axis, notes_in):
